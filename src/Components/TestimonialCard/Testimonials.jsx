fffe23b088f983a3889475b398ea055e090da6a7
Devam Satasiya <109685793+devams23@users.noreply.github.com>
2024-06-16 10:31:07 +0530

    Update Testimonials.jsx

--- src/Components/TestimonialCard/Testimonials.jsx
+++ src/Components/TestimonialCard/Testimonials.jsx
@@ -7,15 +7,15 @@ const Testimonials = () => {
       <h2 className="text-center text-white display-6 fw-bold mb-4">What Customers Say</h2>
       <div className="testimonials-container">
         <div className="testimonial">
-          <p>"The quality of the cereals, pulses, and spices from Delicosa Fz is outstanding. My dishes have never tasted better!"</p>
+          <p>"The quality of the cereals, pulses, and spices from Grains Global  is outstanding. My dishes have never tasted better!"</p>
           <p>~John M.</p>
         </div>
         <div className="testimonial">
-          <p>"I love the freshness and flavor of the products from Delicosa Fz. They're my go-to for quality ingredients."</p>
+          <p>"I love the freshness and flavor of the products from Grains Global. They're my go-to for quality ingredients."</p>
           <p>~Sarah L.</p>
         </div>
         <div className="testimonial">
-          <p>"The best quality ingredients for a healthy diet come from Delicosa Fz. I trust them completely."</p>
+          <p>"The best quality ingredients for a healthy diet come from Grains Global. I trust them completely."</p>
           <p>~Emily R.</p>
         </div>
       </div>
